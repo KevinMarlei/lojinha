c8424a9f65c50286ad867c932437fba19a6ea123
Kevin Marlei <kevin_SSDP@hotmail.com>
2024-11-05 12:48:39 +0100

    Adição de ícones para o botão toggle

--- src/toggleButton/ToggleButton.jsx
+++ src/toggleButton/ToggleButton.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { PiSunThin } from "react-icons/pi";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 function ToggleButton({ isDarkMode, toggleTheme }) {
   return (
-      <button onClick={toggleTheme}>
-        {isDarkMode ? "Modo Claro" : "Modo Escuro"}
-      </button>
+    <button
+      onClick={toggleTheme}
+      className="flex items-center p-2 rounded focus:outline-none transition-all duration-500 ease-in-out"
+    >
+      {isDarkMode ? (
+        <PiSunThin className="w-6 h-6 text-yellow-700 transform scale-125 " title="Tema claro"/>
+      ) : (
+        <BsFillMoonStarsFill className="w-6 h-6 text-blue-500 scale-125" title="Tema escuro"/>
+      )}
+    </button>
   );
 }
 
